2831f72da71777ddf081d1fadc759fc7677f4ae7
david082321 <7046617+david082321@users.noreply.github.com>
2022-04-22 17:46:32 +0800

    Update test.py

--- test.py
+++ test.py
@@ -11,5 +11,5 @@ HTTP_HEADERS = {"user-agent":user_agent,"cookie":cookie}
 response = requests.get(url=url, headers=HTTP_HEADERS).content.decode("utf8")
 if "登录" in str(response):
     requests.get(url="https://api.telegram.org/bot"+bot_token+"/sendMessage?chat_id="+chat_id+"&text=Cookie 出錯")
-elif "访问我的空间" in str(response):
-    requests.get(url="https://api.telegram.org/bot"+bot_token+"/sendMessage?chat_id="+chat_id+"&text=成功")
+#elif "访问我的空间" in str(response):
+#    requests.get(url="https://api.telegram.org/bot"+bot_token+"/sendMessage?chat_id="+chat_id+"&text=成功")
